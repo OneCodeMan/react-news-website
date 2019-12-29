af0725561c718a8f44c278f83b24aa6094530064
Dave Gumba <dave_gumba@Yahoo.com>
2019-12-29 17:42:11 -0500

    there seems to be no way to pass data between two components that do not have a child-parent (or vice versa) relationship. so i need to put two of my components under one child parent.

--- src/Components/OverlayMenu/OverlayMenu.js
+++ src/Components/OverlayMenu/OverlayMenu.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import './OverlayMenu.css';
 
+let flags = [
+  'ar', 'au', 'at', 'be', 'br', 'bg', 'ca', 'cn', 'co', 'cu', 'cz', 'eg', 'fr',
+  'de', 'gr', 'hk', 'hu', 'in', 'id', 'ie', 'il', 'it', 'jp', 'lv', 'lt', 'my',
+  'mx', 'ma', 'nl', 'nz', 'ng', 'no', 'ph', 'pl', 'ro', 'ru', 'sa', 'rs', 'sg',
+  'sk', 'si', 'za', 'kr', 'se', 'ch', 'tw', 'th', 'tr', 'ae', 'ua', 'gb', 'us',
+  've'
+];
+
 class OverlayMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showMenu: false,
+      showMenu: true,
     }
+  }
+
+  getFlagImage(flag) {
+    return 'https://www.countryflags.io/' + flag + '/shiny/64.png';
   }
 
   displayMenu(state) {
     this.setState({ showMenu: state });
   }
+
+  handleFlagClick(flag) {
+    let url = 'https://newsapi.org/v2/top-headlines?country=' + flag +
+              '&apiKey=77e9030d67f34b2e918af2623154e668';
+    // somehow pass this URL to ArticleList.js
+  }
+
   render() {
     return (
       <div>
@@ -24,8 +43,19 @@ class OverlayMenu extends Component {
             &times;
           </a>
           <div className="overlay-menu-content">
-            <a href="#">U.K</a>
-            <a href="#">GERMANY</a>
+            <div className="sub-menu countries-menu">
+              <h1 className="countries-menu-title menu-title">Countries</h1>
+                <div className="flag-list-div">
+                  {flags.map((flag, index) =>
+                    <div className="individual-flag" onClick={() => this.handleFlagClick()}>
+                      <img alt="flag" src={this.getFlagImage(flag)} />
+                    </div>
+                  )}
+                </div>
+            </div>
+            <div className="sub-menu categories-menu">
+              <h1 className="categories-menu-title menu-title">Categories</h1>
+            </div>
           </div>
         </div>
       : ""}
