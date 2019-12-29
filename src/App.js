5da78946e828866978ffad1a5cb7a89512ee4b2e
Dave Gumba <dave_gumba@Yahoo.com>
2019-12-28 21:26:14 -0500

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import ArticleList from './Components/ArticleList/ArticleList';
+import Title from './Components/Title/Title';
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <ArticleList />
     </div>
   );
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () =>
+  <h1>React News</h1>
+
+export default Title;
